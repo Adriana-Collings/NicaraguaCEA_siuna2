bd30b243f760c72b1c61780c239aa7c67119decd
Adriana-Collings <maxxie22>
2018-07-10 14:22:46 -0600

    DALY

--- DALY.py
+++ DALY.py
@@ -1,21 +1,21 @@
-import InputData_Condition1 as D
+import InputDataAC76 as D
 
 # DALY calculation
 YLD_major_comp = 5
 YLL = 10  # do we need different YLL's?
-RD_major_comp = .4
-pMajorComp = .2
 YLD_minor_comp = 3
-pComplications = .6
-PST = .9  # probability of successful treatment
 YLD_disease = 7
-pSurgery = .7
-RD_disease = .8
-pAccess = .9
-DALY = (((((((((YLD_major_comp*(1-RD_major_comp) + (YLL +YLD_major_comp))*pMajorComp) + ((1-pMajorComp)*YLD_minor_comp)) *
-       pComplications) * PST) + ((1-PST)* (YLL + YLD_disease))) * pSurgery) +
-       (1-pSurgery)*((RD_disease*YLL) + ((1-RD_disease)*YLD_disease))) * pAccess)
 
-DALY2 = ((((((YLD_major_comp*(1-D.PR_OS_S_S_C_Major_Die)+ ((YLL+YLD_major_comp)*D.PR_OS_S_S_C_Major_Die)) *
+DALY_OS= ((((((((((YLD_major_comp*(1-D.PR_OS_S_S_C_Major_Die)+ ((YLL+YLD_major_comp)*D.PR_OS_S_S_C_Major_Die)) *
         D.PR_OS_S_S_C_Major) + ((1-D.PR_OS_S_S_C_Major)*YLD_major_comp)) * D.PR_OS_S_S_Comp) +
-        ((1-D.PR_OS_S_S_Comp)*0))* (1-D.PR_OS_S_Die)) + ((D.PR_OS_S_Die)*YLL)
+        ((1-D.PR_OS_S_S_Comp)*0))* (1-D.PR_OS_S_Die)) + ((D.PR_OS_S_Die)*YLL)) * D.PR_OS_A_Surgery) +
+        ((1-D.PR_OS_A_Surgery)*((YLD_disease*(1-D.PR_OS_A_NoSurgery_Die)) + (YLL*D.PR_OS_A_NoSurgery_Die))))*D.PR_OS_Access) +\
+                (((((((((((YLD_major_comp*(1-D.PR_OS_M_S_C_Major_Die)) + ((YLL+YLD_major_comp)*D.PR_OS_M_S_C_Major_Die)) *
+                   D.PR_OS_M_S_C_Major) + ((1-D.PR_OS_M_S_C_Major)*YLD_minor_comp)) * D.PR_OS_M_S_Comp) +
+                    ((1-D.PR_OS_M_S_Comp)*0)) * (1-D.PR_OS_Managua_Die)) + (D.PR_OS_Managua_Die*YLL)) * D.PR_OS_Managua) +
+                   ((1-D.PR_OS_Managua)*((YLD_disease*(1-D.PR_NoOS_NA_D_Die))+((YLL + YLD_disease)*D.PR_NoOS_NA_D_Die)))) *
+                    (1-D.PR_OS_Access))
+
+
+print(DALY_OS)
+
